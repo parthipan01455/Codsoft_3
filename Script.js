90446e9f9ff77ec6bb8a8a36435e6bd65099aca5
Parthipan <parthipanr174@gmail.com>
2026-07-01 14:04:25 +0530

    Add basic calculator functions in Script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+function addValue(value)
+{
+    document.getElementById("display").value += value;
+}
+
+function calculate()
+{
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+}
+
+function clearDisplay()
+{
+    document.getElementById("display").value = "";
+}
